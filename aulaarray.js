0fb82039639a12c0a54d57fdd7a329700abb2b0b
Thiago Ferreira <thiago.rodrigues@sp.senai.br>
2024-03-14 09:23:36 -0300

    mensagem

--- aulaarray.js
+++ aulaarray.js
@@ -2,5 +2,20 @@ let cestaDeFrutas = ['banana', 'maçã', 'manga', 'uva'];
 
 let listaDeCor = ['azul', 'vermelho', 'amarelo', 'preto'];
 
-console.log(cestaDeFrutas);
-console.log(listaDeCor);
+//Exibe a lista completa de arrays, por nome deles né
+//console.log(cestaDeFrutas);
+//console.log(listaDeCor);
+
+//Exibir por posicao ou manipular por posicao.
+
+console.log(cestaDeFrutas[2]); //saida: manga
+
+//manipular um textinho falando que minha fruta
+//favorita é: tal fruta.
+
+let mensagem = 'Minha fruta favorita é: ' + cestaDeFrutas[2];
+let mensagemPlural = 'Minhas frutas favoritas são: ' + cestaDeFrutas;
+console.log(cestaDeFrutas[3]);
+
+console.log(mensagem);
+console.log(mensagemPlural);
