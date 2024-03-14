7001ba9098db80bb85bf7fa788cbf6e40a80954b
Thiago Ferreira <thiago.rodrigues@sp.senai.br>
2024-03-14 09:05:03 -0300

    arrays

--- aulaarray.js
+++ aulaarray.js
@@ -1,12 +1,12 @@
-//aula array de javascript.
-//teste 1 de comentario
-//teste 1
-//teste 2
-//teste 3
-//teste 4
-//teste 5
-//teste 6
-//teste 7
-//teste 8
-// Alteracao feita por Thiago Ferreira
-// outra alteracao
+let cestaDeFrutas = [];
+let fruta = 'banana';
+let quantidadedeFruta = 110;
+
+//typeof
+//console.log(typeof(cestaDeFrutas));
+//console.log(typeof(fruta));
+//console.log(typeof(quantidadedeFruta));
+
+
+
+
